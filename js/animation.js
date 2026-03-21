3429ca4e1e8324a61dbdaac8410c0f7197ed5979
GavTechMaster <m67115586@gmail.com>
2026-03-21 16:10:57 -0500

    Added Boombox Animation

---
+++ js/animation.js
@@ -0,0 +1,28 @@
+var x = 0;
+var y = 0;
+var frame = 0;
+
+function gavinwareAnimation() {
+    var gavinwareSpritesheet = document.getElementById("gavinware-animation");
+    gavinwareSpritesheet.style.backgroundPosition = `-${x * 75}px -${y * 85}px`;
+    if (frame < 16) {
+        if (x < 6) {
+            x++;
+            frame++;
+        } else {
+            x = 0;
+            if (y < 1) {
+                y++;
+            } else {
+                y = 0;
+            }
+            frame++;
+        }
+    } else {
+        x = 0;
+        y = 0;
+        frame = 0;
+    }
+}
+
+setInterval(gavinwareAnimation, 1000 / 40);
